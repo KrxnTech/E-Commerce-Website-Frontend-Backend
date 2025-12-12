dc97c11d7f50d57d0035068238f6eac756684ef5
KrxnTech <sharmakrishna1142@gmail.com>
2025-12-12 20:58:27 +0530

    Update comments for cart routes

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -5,14 +5,13 @@ const cartController = require('../controllers/cartController')
 // ADD TO CART
 router.post('/', cartController.addToCart)
 
-// TO SHOW ALL ITEMS FROM THE CART
+// GET ALL CART ITEMS
 router.get('/', cartController.getCartItems)
 
-// UPDATE THE QUANTITY OF THE PRODUCT WHICH IS ALREADY IN THE CART
+// UPDATE CART ITEM
 router.put('/:id', cartController.updateCartItem)
 
-// DELETE THE PRODUCT FROM THE CART BY TRACKING THE ID OF THE PRODUCT WHICH IS GIVEN BY THE CART 
+// DELETE CART ITEM
 router.delete('/:id', cartController.deleteCartItem)
-
 
 module.exports = router
