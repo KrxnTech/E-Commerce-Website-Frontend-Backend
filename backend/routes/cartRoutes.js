be4a8037d44f41b1146b8ca5b36ef616d02f3e48
KrxnTech <sharmakrishna1142@gmail.com>
2025-12-12 20:36:15 +0530

    Refactor cart routes to use controller methods

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const cartController = require('../controllers/cartController')
 
-router.get('/', (req, res) => {
-  res.send("Cart route working!")
-})
+// ADD TO CART
+router.post('/', cartController.addToCart)
+
+// TO SHOW ALL ITEMS FROM THE CART
+router.get('/', cartController.getCartItems)
+
+// UPDATE THE QUANTITY OF THE PRODUCT WHICH IS ALREADY IN THE CART
+router.put('/:id', cartController.updateCartItem)
+
+// DELETE THE PRODUCT FROM THE CART BY TRACKING THE ID OF THE PRODUCT WHICH IS GIVEN BY THE CART 
+router.delete('/:id', cartController.deleteCartItem)
+
 
 module.exports = router
